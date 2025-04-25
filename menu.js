acd6f8f715f36f5964666756e6226fd7f89c2a06
TimGorelov12 <gorelov_timofey@mail.ru>
2025-04-26 01:49:53 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,10 +2,11 @@ function toggleMenu() {
   document.getElementById("sidebar").classList.toggle("open");
 }
 
-// Закрытие меню при клике вне него
+// Закрытие меню при клике вне него и вне кнопки
 document.addEventListener("click", function (event) {
   const sidebar = document.getElementById("sidebar");
-  const burger = document.querySelector(".menu-btn"); // ← здесь важно!
+  const burger = document.querySelector(".menu-btn"); // ← это твоя кнопка ☰
+
   const isClickInsideSidebar = sidebar.contains(event.target);
   const isClickOnBurger = burger && burger.contains(event.target);
 
